9d97d68e80d7bb6c39a28369f3944eb449d72787
gabrielyuskesato <gabrielyuskesato@icloud.com>
2022-01-27 16:40:10 +0900

    adcionei as funcao de cadastro de produto e listagem de produto

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -1,28 +1,31 @@
-from typing import Optional
+
 from pydantic import BaseModel
-from typing import Optional,List
+from typing import Optional, List
 
 class User(BaseModel):
     id: Optional[str] = None
     nome: str
     telefone: str
-    meus_produtos: List[Order]
-    minhas_vendas: List[Pedido]
-    meus_pedidos: List[Pedido]
+    # meus_produtos: List[Produto]
+    # minhas_vendas: List[Pedido]
+    # meus_pedidos: List[Pedido]
 
 
-class Product(BaseModel):
+class Produto(BaseModel):
     id:Optional[str] = None
-    usuario: User 
     nome: str
     detalhes: str
     preco: float
     disponivel: bool = False
 
-class Order(BaseModel):
+    class Config:
+        orm_mode = True
+        
+
+class Pedido(BaseModel):
     id:Optional[str] = None
     usuario : User
-    produto : Product
+    # produto : Produto
     entrega : bool = True
     endereco: str
     observacao : Optional[str] = "Sem observacao"
